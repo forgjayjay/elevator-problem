440504bf7e39223147f49ef16d9dfd79da3d133f
forgge95 <edlink2004@gmail.com>
2022-11-20 14:41:43 +0100

    styling

--- src/main/java/com/forg/App.java
+++ src/main/java/com/forg/App.java
@@ -2,24 +2,28 @@ package com.forg;
 
 public class App 
 {
-    public static void main( String[] args ) throws InterruptedException
+    public static void main( String[] args )
     {
         //Building building = new Building(10);
         ElevatorHandler handler = new ElevatorHandler();
         for (int i = 0; i < 3; i++) {
             ElevatorHandler.elevators.add(new Elevator());
         }
-        System.out.println(ElevatorHandler.elevators);
-        handler.freeElevator().move(2);
-        System.out.println(ElevatorHandler.elevators);
-        handler.freeElevator().move(5);
-        System.out.println(ElevatorHandler.elevators);
-        handler.freeElevator().move(10);
-        Thread.sleep(2000);
-        System.out.println(ElevatorHandler.elevators);
-        Thread.sleep(5000);
-        System.out.println(ElevatorHandler.elevators);
-        handler.freeElevator().move(5);
-        System.out.println(ElevatorHandler.elevators);
+        try {
+            System.out.println(ElevatorHandler.elevators);
+            handler.freeElevator().move(2);
+            System.out.println(ElevatorHandler.elevators);
+            handler.freeElevator().move(5);
+            System.out.println(ElevatorHandler.elevators);
+            handler.freeElevator().move(10);
+            Thread.sleep(2000);
+            System.out.println(ElevatorHandler.elevators);
+            Thread.sleep(5000);
+            System.out.println(ElevatorHandler.elevators);
+            handler.freeElevator().move(5);
+            System.out.println(ElevatorHandler.elevators);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
